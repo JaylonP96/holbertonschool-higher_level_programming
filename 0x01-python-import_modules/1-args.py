d2194a46dab85f8ae2ce03c61f75b1bc54ee5f4a
Jaylon <jaylonp96@gmail.com>
2023-01-22 00:37:21 +0000

    1-args.py

---
+++ 0x01-python-import_modules/1-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+import math
+
+if __name__ == '__main__':
+    numarg = len(sys.argv) - 1
+    if numarg == 1:
+        print('{} argument:'.format(numarg))
+    else:
+        print('{} arguments:'.format(numarg))
+    for i in range(1, numarg + 1):
+        print('{}: {}'.format(i, sys.argv[i]))
