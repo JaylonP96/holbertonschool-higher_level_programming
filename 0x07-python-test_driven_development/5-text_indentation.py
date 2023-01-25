9bd889ecaa56a89aad09c1384bc38d23c99fe727
Jaylon <jaylonp96@gmail.com>
2023-01-25 17:09:51 +0000

    5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,20 +1,51 @@
 #!/usr/bin/python3
 """
-This is the text_indentation module.
-The text_indentation module prints a text with 2
-new lines after each of these characters: ., ? and :
+This module prints a text with a 2 new lines after each of
+these characters: `.`, `?`, `:`
+
+    * text must be a string
+    * There should be no space at the beginning or
+    at the end of each printed line
 """
 
 
 def text_indentation(text):
     """
-    Function that prints a text with 2
-    new lines after each of these characters: ., ? and :
+    Prints a text with indentation
+    Args:
+        text (str): The text to prints.
+    Raises:
+        TypeError: If `text` isn't string.
     """
-    if type(text) not in [str]:
-        raise TypeError("text must be a string")
-    for i in text:
-        if i in ".?:":
-            print(i, end="\n\n")
-        else:
-            print(i, end="")
+
+    if type(text) is not str:
+        raise TypeError('text must be a string')
+
+    text_length = len(text)
+    idx = 0
+    new_string = ''
+    starting = True
+
+    while idx < text_length:
+        if text[idx] == ' ' and starting is True:
+            idx += 1
+            continue
+
+        starting = False
+
+        if text[idx] in '.?:':
+            new_string += text[idx]
+            new_string += '\n'
+            new_string += '\n'
+            idx += 1
+
+            while idx < text_length and text[idx] == ' ':
+                idx += 1
+
+            continue
+
+        if idx < text_length:
+            new_string += text[idx]
+            idx += 1
+
+    print(new_string, end='')
